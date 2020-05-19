de3b3b0a993b2768d328aa8f322aa099643b31f4
binjia <mangosata@gmail.com>
2020-05-19 17:24:02 -0400

    first version

---
+++ cmake-build-debug/CMakeFiles/Group_D_Gomoku.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Group_D_Gomoku.dir/main.c.obj"
+  "Group_D_Gomoku.exe"
+  "Group_D_Gomoku.exe.manifest"
+  "Group_D_Gomoku.pdb"
+  "libGroup_D_Gomoku.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Group_D_Gomoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Group_D_Gomoku.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Group_D_Gomoku.
+# This may be replaced when dependencies are built.
